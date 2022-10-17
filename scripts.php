2793621e22f1ea819b0cfcaa31d7f77a3df112b4
gacvillalta34 <gacvillalta34@gmail.com>
2022-10-17 09:58:21 -0600

    Footer

---
+++ scripts.php
@@ -0,0 +1,22 @@
+ <!-- Back to top -->
+    <a href="#" id="back-to-top" title="Back to top"><i class="fa fa-angle-up"></i></a>
+    <!-- /Back to top -->    
+    <!-- jQuery -->
+    <script src="diseno/js/jquery.min.js"></script>    
+    <!-- Bootstrap -->
+    <script src="diseno/bootstrap/js/bootstrap.min.js"></script>   
+    <!-- Components Plugin -->
+    <script src="diseno/js/jquery.easing.1.3.js"></script>
+    <script src="diseno/js/smooth-scroll.js"></script>
+    <script src="diseno/js/jquery.appear.js"></script>
+    <script src="diseno/js/jquery.countTo.js"></script>
+    <script src="diseno/js/jquery.stellar.min.js"></script>
+    <script src="diseno/js/jquery.magnific-popup.min.js"></script>
+    <script src="diseno/js/imagesloaded.pkgd.min.js"></script>
+    <script src="diseno/js/isotope.pkgd.min.js"></script>
+    <script src="diseno/js/jquery.mb.YTPlayer.js"></script>
+    <script src="diseno/js/retina.min.js"></script>
+    <script src="diseno/js/wow.min.js"></script>
+    <script src="diseno/inc/owlcarousel/js/owl.carousel.min.js"></script>   
+    <!-- Custom Plugin -->
+    <script src="diseno/js/custom.js"></script>   
