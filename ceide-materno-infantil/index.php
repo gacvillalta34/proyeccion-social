6e37ff21e2f8fdb0406e40f513384d55d3948100
gacvillalta34 <Aaronvillalta30>
2023-01-03 12:51:45 -0600

    Modificación año 2023

---
+++ ceide-materno-infantil/index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title>CEIDE - MATERNO E INFANTIL</title>
+    <meta name="description" content="N.Agency - Responisve Landing Page for Agency">
+    <meta name="keywords" content="">
+    <meta name="author" content="tabthemes">
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="../logos-centros-de-encuentros/proyeccion-logo.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="../diseño/img/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../diseño/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../diseño/img/apple-touch-icon-114x114.png">   
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">  
+    <!-- Bootstrap CSS -->
+    <link href="../diseño/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- CSS Files For Plugin -->
+    <link href="../diseño/css/animate.css" rel="stylesheet">
+    <link href="../diseño/css/font-awesome/font-awesome.min.css" rel="stylesheet">
+    <link href="../diseño/css/magnific-popup.css" rel="stylesheet" />
+    <link href="../diseño/css/YTPlayer.css" rel="stylesheet" /> 
+    <!-- Custom CSS -->
+    <link href="../diseño/css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+   <body class="portfolio_page_2col" data-spy="scroll" data-target=".navbar-fixed-top" data-offset="100">
+    <!-- Preloader -->
+    <div id="preloader">
+        <div id="spinner"></div>
+    </div>
+    <!-- End Preloader-->  
+    <!-- Start Navigation -->
+    <header class="nav-solid" id="home">
+        <nav class="navbar navbar-fixed-top">
+            <div class="navigation">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <!-- Logo -->
+                            <div class="logo-container">
+                                <div class="logo-wrap local-scroll">
+                                  <a href="../">
+                                    <img class="logo" src="../logos-centros-de-encuentros/proyeccion-logo.png" height="50" width="50">
+                                  </a>
+                                </div>
+                            </div>
+                        </div> <!-- end navbar-header -->
+                        <div class="col-md-8 col-xs-12 nav-wrap">
+                            <div class="collapse navbar-collapse" id="navbar-collapse">
+							<ul class="nav navbar-nav navbar-right">
+                                    <li><a href="../">Inicio</a></li>
+                                    <li><a href="../cedi-juvenil">JUVENIL</a></li>
+                                    <li><a href="../ceide-hombre">HOMBRE</a>
+                                    <li><a href="../ceide-mujer"> MUJER</a></li>
+                                    <li><a href="../ceide-ninez">NIÑEZ</a></li>
+                                    <li><a href="">MATERNO - INFANTIL</a></li>                       
+                                    <li><a href="../ceide-primera-infancia">PRIMERA INFANCIA</a></li>                       
+                                    <li><a href="../salud-integral">SALUD INTEGRAL</a></li>
+                                </ul>
+                            </div>
+                        </div> <!-- /.col -->
+                    </div> <!-- /.row -->
+                </div> <!--/.container -->
+            </div> <!-- /.navigation-overlay -->
+        </nav> <!-- /.navbar -->
+    </header>
+    <!-- End Navigation -->
+    <!-- Section Title Portfolio -->
+    <div class="section-title-bg text-center">
+        <h2 class="wow fadeInDown no-margin" data-wow-duration="1s" data-wow-delay="0.6s"><strong>
+		Centro Integral de Desarrollo Materno - Infantil</strong></h2>
+       </div>
+    <!-- Start Portfolio -->
+    <section id="portfolio" class="p-top-80 p-bottom-80">
+        <div class="container">
+		<img class="img-responsive" src="../logos-centros-de-encuentros/ceide-materno-infantil.png" height="550" width="450"/>
+            <!-- Portfolio-filter -->
+            <ul class="pf-filter pf-filter-gray text-center list-inline">
+                <li><a href="#" data-filter="*" class="iso-active iso-button">Todas</a></li>
+                <li><a href="#" data-filter=".programacion" class="iso-button">Programación</a></li>
+                <li><a href="#" data-filter=".informatica" class="iso-button">Informática</a></li>
+                <li><a href="#" data-filter=".robotica" class="iso-button">Robótica</a></li>
+                <li><a href="#" data-filter=".ingles" class="iso-button">Inglés</a></li>
+                <li><a href="#" data-filter=".dibujo-arte" class="iso-button">Dibujo y arte</a></li>
+                <li><a href="#" data-filter=".musica" class="iso-button">Música</a></li>
+            </ul>          
+            <!-- Portfolio -->
+            <div class="portfolio portfolio-isotope col-2 gutter">
+                <!-- Portfolio Item -->
+                <div class="pf-item programacion">
+                    <a href="../cedi-juvenil/programacion" >
+                        <div class="pf-image">
+                            <img class="img-responsive" src="../diseño/images/cursos/programacion-1.gif" width="150" height="450">
+                            </div> <!-- .pf-image -->
+                    </a> <!-- .pf-style -->
+                </div>
+                <!-- Portfolio Item -->
+                <div class="pf-item informatica webdesign">
+                    <a href="../cedi-juvenil/informatica" >
+                        <div class="pf-image">
+                            <img class="img-responsive" src="../diseño/images/cursos/computación1.gif" width="150" height="450">
+                            </div> <!-- .pf-image -->
+                    </a> <!-- .pf-style -->
+                </div>
+                <!-- Portfolio Item -->
+                <div class="pf-item robotica webdesign">
+                    <a href="../cedi-juvenil/robotica">
+                        <div class="pf-image">
+                            <img class="img-responsive" src="../diseño/images/cursos/robotica-1.gif" width="150" height="450">
+                            </div> <!-- .pf-image -->
+                    </a> <!-- .pf-style -->
+                </div>
+                <!-- Portfolio Item -->
+                <div class="pf-item ingles webdesign">
+                    <a href="../cedi-juvenil/ingles">
+                        <div class="pf-image">
+                            <img class="img-responsive" src="../diseño/images/cursos/ingles-1.gif" width="150" height="450">
+                           </div> <!-- .pf-image -->
+                    </a> <!-- .pf-style -->
+                </div>
+                <!-- Portfolio Item -->
+                <div class="pf-item dibujo-arte graphic">
+                    <a href="../cedi-juvenil/dibujo-pintura" >
+                        <div class="pf-image">
+                            <img class="img-responsive" src="../diseño/images/cursos/arte-2.gif" width="150" height="450">
+                            </div> <!-- .pf-image -->
+                    </a> <!-- .pf-style -->
+                </div>
+                <!-- Portfolio Item -->
+                <div class="pf-item musica graphic">
+                    <a href="../cedi-juvenil/musica">
+                        <div class="pf-image">
+                            <img class="img-responsive" src="../diseño/images/cursos/musica-2.gif" width="150" height="450">
+                            </div> <!-- .pf-image -->
+                    </a> <!-- .pf-style -->
+                </div>
+            </div> <!-- Portfolio -->
+        </div> <!-- /.container -->
+    </section>
+    <!-- End Portfolio -->
+    <!-- Start Call to Action -->
+    <section class="light-bg p-top-60 p-bottom-60">
+        <div class="container">          
+            <div class="row">              
+                <div class="col-xs-12 c2a-content text-center">                   
+                    <!-- Text -->
+                    <h2 class="wow fadeInDown" data-wow-delay="0.3s">¿Quieres saber sobre nosotros?</h2>
+                    <!-- Button -->
+                    <a data-scroll href="index_02.html#price" class="btn btn-main btn-theme wow fadeInUp" data-wow-delay="0.5s">Click aquí</a>            
+                </div> <!-- /.col -->
+            </div>  <!-- /.row -->          
+        </div> <!-- /.container -->
+    </section>
+    <!-- End Call to Action -->
+    <!-- Start Footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <small class="copyright pull-left">Alcaldía Municipal de San Miguel<a href="http://www.tabthemes.com/"></a></small>
+            <small class="copyright pull-right">Plantilla desarrollada por:&nbsp;<a href="http://www.tabthemes.com/">tabthemes</a></small>
+            <small class="copyright pull-right"><a href="http://www.tabthemes.com/">|&nbsp;</a></small>
+            <small class="copyright pull-right">Página desarrollada por:&nbsp;<a href="https://www.facebook.com/aaron.villalta.3994">Aarón Villalta &nbsp;</a></small>
+			<br>
+			<br>
+           <div class="social-icon pull-center">
+                <a href="https://www.facebook.com/proyeccionsocialsm"><i class="fa fa-facebook"></i></a>
+            </div>
+            <!-- /social-icon -->
+        </div>
+        <!-- /container -->
+    </footer>
+    <!-- End Footer -->
+    <!-- Back to top -->
+    <a href="#" id="back-to-top" title="Back to top"><i class="fa fa-angle-up"></i></a>
+    <!-- /Back to top -->    
+    <!-- jQuery -->
+    <script src="../diseño/js/jquery.min.js"></script>   
+    <!-- Bootstrap -->
+    <script src="../diseño/bootstrap/js/bootstrap.min.js"></script>   
+    <!-- Components Plugin -->
+    <script src="../diseño/js/jquery.easing.1.3.js"></script>
+    <script src="../diseño/js/smooth-scroll.js"></script>
+    <script src="../diseño/js/jquery.appear.js"></script>
+    <script src="../diseño/js/jquery.countTo.js"></script>
+    <script src="../diseño/js/jquery.stellar.min.js"></script>
+    <script src="../diseño/js/jquery.magnific-popup.min.js"></script>
+    <script src="../diseño/js/imagesloaded.pkgd.min.js"></script>
+    <script src="../diseño/js/isotope.pkgd.min.js"></script>
+    <script src="../diseño/js/jquery.mb.YTPlayer.js"></script>
+    <script src="../diseño/js/retina.min.js"></script>
+    <script src="../diseño/js/wow.min.js"></script>
+    <script src="../diseño/inc/owlcarousel/js/owl.carousel.min.js"></script>   
+    <!-- Custom Plugin -->
+    <script src="../diseño/js/custom.js"></script>   
+  </body>
+</html>
